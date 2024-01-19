2fa0158c0ca2bd0e4c51a113f1d311cc3167445f
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2024-01-19 19:47:59 +0000

    Add config

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,10 @@
-
+import { site } from '@/lib/config'
 
 export default function Footer() {
     const Year =  new Date().getFullYear()
     return (
         <footer className="primary-footer">
-        <p>Copyright &copy; {Year}</p>
+        <p>Copyright &copy; {Year} {site.author}</p>
       </footer>
     );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,21 @@
-
+import { site, navLinks } from '@/lib/config'
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <header className="primary-header">
-        <a href="/" className="site__logo">Site Name</a>
-        <nav>
-          <ul className="site__nav">
-            <li><a href="">Lorem.</a></li>
-            <li><a href="">Molestiae.</a></li>
-            <li><a href="">Odio.</a></li>
-          </ul>
-        </nav>
-      </header>
+            <a href="/" className="site__logo">{site.title}</a>
+            <nav>
+                <ul className="site__nav">
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <Link href={link.url}>
+                                {link.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
     );
 }
