d032b8aefd890e15b262c35261dda47f64400bcd
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2024-01-25 21:49:33 +0000

    few updates

--- app/page.js
+++ app/page.js
@@ -7,12 +7,40 @@ export default function Home() {
    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus autem magni nemo libero vero! Fuga atque quasi quibusdam quidem optio ex, quos id recusandae. Aliquam id amet incidunt quas sapiente.</p>
    <p>Earum, sapiente. Saepe illum vel asperiores, neque aliquid placeat nam officia labore alias suscipit doloremque. Iusto eos sint ipsum cumque quibusdam, ex vitae. Quae mollitia, eum facere modi nulla odit!</p>
    <h2>Lorem, ipsum.</h2>
+   <blockquote>
+    <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus beatae eaque veritatis odio aliquam itaque neque eum nostrum nisi ipsam laboriosam, porro nobis aliquid possimus dolore, harum cumque nemo vero?</p>
+   </blockquote>
    <p>Amet quos aspernatur tempore quidem officiis dolores, recusandae accusamus aliquam possimus corrupti labore expedita veniam ab placeat incidunt dolore quibusdam consequuntur animi ut quae soluta qui itaque aliquid. Eum, cupiditate?</p>
    <h3>Lorem, ipsum dolor.</h3>
    <p>Nulla consequatur exercitationem possimus similique, illo sint esse libero amet quisquam ad, at ipsam vero! Exercitationem architecto, vitae reiciendis cupiditate molestiae voluptate provident accusamus soluta eum illo, sapiente ex vel!</p>
    <h4>Lorem ipsum dolor sit.</h4>
    <p>Nostrum, culpa voluptates natus quasi, odit unde fuga voluptas autem sapiente placeat quo debitis nisi modi provident nam impedit ut accusantium laborum sed temporibus illo esse, asperiores porro itaque. Optio.</p>
    <p>Ipsa quasi aut, voluptatem officia, maiores corrupti cupiditate iure reprehenderit perferendis soluta beatae quisquam eligendi fugit adipisci deleniti! Ut dolorum ad labore, expedita asperiores ullam aliquam et eveniet voluptatibus incidunt?</p>
+   
+   <div className="card">
+    <h2>Card</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+   </div>
+
+   <div className="card__group">
+   <div className="card">
+    <h2>Card</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+   </div>
+   <div className="card">
+    <h2>Card</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+   </div>
+   <div className="card">
+    <h2>Card</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+   </div>
+   <div className="card">
+    <h2>Card</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+   </div>
+   
+   </div>
    </>
   )
 }
